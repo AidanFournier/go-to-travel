6c406e37432e9478c9c30aa1dcfa7059e066dad4
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-03-15 22:50:39 +0900

    Added home screen to navigation

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaView, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View className={`text-violet-500`}>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
