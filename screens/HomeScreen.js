9daae156b83af79e1987a76ee1ba96b471bcf418
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-03-16 23:04:15 +0900

    Added logo to header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
-  return (
-    <View className={`text-violet-500`}>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, [])
+
+    return (
+        <SafeAreaView className="bg-white flex-1 relative">
+            {/* Header */}
+            <View className="flex-row px-6 mt-8 items-center space-x-2">
+                <View className="w-16 h-16 bg-[#f78172] rounded-full items-center justify-center">
+                    <Text className="text-white text-3xl font-semibold">Go</Text>
+                </View>
+                <Text className="text-[#E8AF0A] text-3xl font-semibold">To Travel</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
 
 export default HomeScreen
