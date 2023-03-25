ed0b56e98f613ad1e3d1f68f4d93227d8b1dbfd8
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-03-25 22:51:43 +0900

    Added link to discover screen

---
+++ screens/DiscoverScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Discover = () => {
+  return (
+    <View>
+      <Text>Discover</Text>
+    </View>
+  )
+}
+
+export default Discover
