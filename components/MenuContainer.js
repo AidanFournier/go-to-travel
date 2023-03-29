f50be4deae5c7655faa14e2c1af277f3022e14b3
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-03-29 23:31:47 +0900

    Created menu container

---
+++ components/MenuContainer.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MenuContainer = ({ title, imageSrc, type, setType }) => {
+  return (
+    <View>
+      <Text>MenuContainer</Text>
+    </View>
+  )
+}
+
+export default MenuContainer
--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,14 +1,17 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 import { REACT_NATIVE_GOOGLE_PLACES_API_KEY } from "@env";
-import { Avatar } from '../assets';
+import { Avatar, HotelIcon } from '../assets';
+import MenuContainer from '../components/MenuContainer';
 
 const Discover = () => {
     
     const navigation = useNavigation() ;
+
+    const [type, setType] = useState("restaurants")
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -47,8 +50,22 @@ const Discover = () => {
                     onFail={(error) => console.error(error)}
                 />
             </View>
+
+            {/* Menu Container */}
+            <ScrollView>
+                <View className="flex-row items-center justify-center px-8 mt-8">
+                    <MenuContainer 
+                        key={"hotel"}
+                        title="Hotels"
+                        imageSrc={HotelIcon}
+                        type={type}
+                        setType={setType}
+                    />
+                </View>
+            </ScrollView>
+
         </SafeAreaView>
     )
-}
+};
 
-export default Discover
+export default Discover;
