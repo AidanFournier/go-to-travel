572e9409ad2464c8d77f4989ee6adda4c4110873
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-04-08 00:21:30 +0900

    Completed show screen image card

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -32,14 +32,41 @@ const ItemScreen = ({ route }) => {
                     />
 
                     <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
-                        <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-white">
+                        <TouchableOpacity 
+                            className="w-10 h-10 rounded-md items-center justify-center bg-white" 
+                            onPress={() => navigation.navigate("Discover")}
+                        >
                             <Image source={ChevronLeft} className="w-8 h-8 object-cover"/>
                         </TouchableOpacity>
                         <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#336699]">
                             <Image source={WhiteHeart} className="w-8 h-8 object-cover"/>
                         </TouchableOpacity>
                     </View>
+
+                    <View className="absolute flex-row inset-x-0 bottom-5 justify-between px-6">
+                    
+                        <View className="flex-column items-start">
+                            <Text className="text-[32px] font-bold text-gray-100">
+                                {data?.price}
+                            </Text>
+                            <Text className="text-[12px] font-bold text-gray-100">
+                                {data?.price_level}
+                            </Text>
+                        </View>
+
+                        <View className="px-2 rounded-md bg-white items-center justify-center">
+                            {data?.open_now_text === "Open Now" ? 
+                                <Text className="text-[#336699] font-bold px-2">{data?.open_now_text}</Text>
+                                :
+                                <Text className="text-[#E77587] font-bold px-2">{data?.open_now_text}</Text>
+                            }
+                        </View>
+                            
+                        
+                    </View>
                 </View>
+
+
             </ScrollView>
         </SafeAreaView>
     );
