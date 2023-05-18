ac6dee357a7fed2334dbbbb5be408cbd5a45f7d2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-05-18 22:52:32 +0900

    Cleaned up code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -65,57 +65,9 @@ const HomeScreen = () => {
                     </Animatable.View>
                 </TouchableOpacity>
             </View>
-
-            
-
-
-            {/* <View className="flex-row px-6 mt-4 items-center space-x-2">
-                <View className="w-16 h-16 bg-[#336699] rounded-full items-center justify-center">
-                    <Text className="text-white text-3xl font-semibold">Go</Text>
-                </View>
-                <Text className="text-[#336699] text-3xl font-semibold">To Travel</Text>
-            </View> */}
-
-            {/* Subtext */}
-            {/* <View className="px-6 mt-8 space-y-3"> */}
-                {/* <Text className="text-[#E77587] text-[42px]">Are you ready to see Japan?</Text> */}
-                
-                {/* <Text className="text-base">Enjoy yourself, and let us handle the details. With Go To Travel, planning your dream trip becomes a breeze.</Text> */}
-            {/* </View> */}
-
-            {/* Image Container */}
-            {/* <View className="flex-1 relative items-center justify-center"> */}
-                {/* <Image 
-                    source={HeroImage}
-                    className="object-contain"
-                />
-                <Animatable.Image 
-                    animation="slideInRight"
-                    easing="ease-in"
-                    source={HeroImageSlide1}
-                    className="absolute"
-                />
-                <Animatable.Image 
-                    animation="slideInUp"
-                    easing="ease-in"
-                    source={HeroImageSlide2}
-                    className="absolute"
-                /> */}
-
-                {/* CTA Button */ }
-                {/* <Animatable.View animation={"pulse"} easing="ease-in-out" iterationCount={"infinite"} className="absolute bottom-70">
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("Discover")}
-                    >
-                        <LinearGradient colors={['#8AC6D3', '#C2E6B1']} className="w-[110] h-[100] rounded-full items-center justify-center">
-                            <Text className="text-white text-[40px] font-semibold">Go</Text>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </Animatable.View>  */}
-            {/* /* </View> */}
             
         </SafeAreaView>
     );
 };
 
-export default HomeScreen
+export default HomeScreen;
