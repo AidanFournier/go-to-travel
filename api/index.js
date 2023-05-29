4adcd7f7453a3226dbc33e6495e94801cbc8a3d2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-05-29 23:36:50 +0900

    Solved issue with missing geoCoord bl_lat

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { REACT_NATIVE_TRAVEL_ADVISOR_API_KEY } from "@env"
 
-export const getPlacesData = async ( { geoCoords: bl_lat, bl_lng, tr_lat, tr_lng }, type ) => {
+export const getPlacesData = async ( { bl_lat, bl_lng, tr_lat, tr_lng }, type ) => {
     
     const url = `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`;
-    
+
     const headers = {
         'X-RapidAPI-Key': REACT_NATIVE_TRAVEL_ADVISOR_API_KEY,
         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
@@ -15,10 +15,8 @@ export const getPlacesData = async ( { geoCoords: bl_lat, bl_lng, tr_lat, tr_lng
         tr_latitude: tr_lat ? tr_lat : '36.4408483',
         bl_longitude: bl_lng ? bl_lng : '138.2991098',
         tr_longitude: tr_lng ? tr_lng : '141.2405144',
-        restaurant_tagcategory_standalone: '10591',
-        restaurant_tagcategory: '10591',
         limit: '30',
-        currency: 'USD',
+        currency: 'JPY',
         open_now: 'false',
         lunit: 'km',
         lang: 'en_US'
